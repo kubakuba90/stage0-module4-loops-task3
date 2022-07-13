84c09de35fc8088acff2f6257da03694981ebbf3
Jakub M <fotograflubin@gmail.com>
2022-07-13 08:26:55 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,9 +2,7 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-
         int n = lastFibonacci, firstTerm = 0, secondTerm = 1;
-       // System.out.println("Fibonacci Series till " + n + " terms:");
 
         for (int i = 1; i <= n; ++i) {
             System.out.println(firstTerm);
@@ -15,8 +13,4 @@ public class FibonacciSeries {
             secondTerm = nextTerm;
         }
     }
-
-//    public static void main(String[] args) {
-//        printFibonacci(5);
-//    }
 }
