5152a6b777f65c0d0894018364dc14a2580799c7
Jakub M <fotograflubin@gmail.com>
2022-07-13 08:33:03 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -3,19 +3,20 @@ package school.mjc.stage0.loops.task3;
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
 
-        while(first!=second)
-        {
-            if(first>second) {
-                first = first - second;
+        if (first == 0) {
+            System.out.println(second);
+        } else if (second == 0) {
+            System.out.println(first);
+        } else {
+            while(first!=second) {
+                if(first>second) {
+                    first = first - second;
+                } else
+                    second=second-first;
             }
-            else if (first == 0) {
-                System.out.println(second);
-            } else if (second == 0) {
-                System.out.println(first);
-            } else
-                second=second-first;
+            System.out.println(second);
         }
-        System.out.println(second);
+
     }
 }
 
