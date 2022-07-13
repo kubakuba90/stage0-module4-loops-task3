f5e1df4d052f273a040481dc12209d6c896a27b0
Jakub M <fotograflubin@gmail.com>
2022-07-13 08:46:01 +0200

    done

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -1,16 +1,16 @@
 package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
-    public void printDigitsSum(int t){
+    public static void printDigitsSum(int t){
+        int tPos = Math.abs(t);
 
-        String tStr = String.valueOf(t);
+        String tStr = String.valueOf(tPos);
         char[] ch = tStr.toCharArray();
         int sum = 0;
         for (int i = 0; i < ch.length; i++) {
-            sum = sum + ch[i];
+            sum = sum + Character.getNumericValue(ch[i]);
         }
-
         System.out.println(sum);
-
     }
+
 }
